0dc12a9f581cbed94439e39f8ae2e8b496f6f53f
FelitoAguila <feloaguila@gmail.com>
2026-02-12 13:34:34 -0300

    Fix typo in global_ammount_order_status.sql comment
    Corrected a comment typo in the SQL query.

--- queries/global_ammount_order_status.sql
+++ queries/global_ammount_order_status.sql
@@ -1,4 +1,4 @@
--- TODO: This query will return a table with two columns; order_status, and
+-- This query will return a table with two columns; order_status, and
 -- Ammount. The first one will have the different order status classes and the
 -- second one the total ammount of each.
 
